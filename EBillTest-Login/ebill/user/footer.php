8f800296f256121906cd847078c052b43a50e903
Filmer Romero <filmer.romero@outlook.com>
2022-12-09 15:57:54 +0800

    Revert "Dashboard changes"
    This reverts commit 3f45f8482bf9edaab50840541b7a7922c32a3d3e.

--- EBillTest-Login/ebill/user/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- FOOTER -->
-    <footer class="text-center">
-        <div class="footer-below">
-            <div class="container">
-                Created by <a href="">Billing </a> System
-            </div>
-        </div>
-    </footer>
